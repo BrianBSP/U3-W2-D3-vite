cc78db88a0bc4a79d02586ad4c01b99116d5cd0e
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-07-03 16:30:25 +0200

    aggiunto primo percorso

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import MyNavbar from "./components/MyNavbar";
 import MainSection from "./components/MainSection";
 import MyFooter from "./components/MyFooter";
 import GalleriaFilms from "./components/GalleriaFilms";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <MyNavbar />
-      <MainSection />
-      <GalleriaFilms />
-      <MyFooter />
+      <BrowserRouter>
+        <MyNavbar />
+        <Routes>
+          <Route path="/" element={<MainSection />} />
+          <Route path="/gallerie" element={<GalleriaFilms />} />
+        </Routes>
+        <MyFooter />
+      </BrowserRouter>
     </div>
   );
 }
